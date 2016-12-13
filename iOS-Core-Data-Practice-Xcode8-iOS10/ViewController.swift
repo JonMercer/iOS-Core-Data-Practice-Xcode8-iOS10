9ef16399bac166ebe27055e219f48688ee599768
JonMercer <mercer.jon@gmail.com>
2016-12-12 18:46:24 -0800

    initial setup of project

---
+++ iOS-Core-Data-Practice-Xcode8-iOS10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Core-Data-Practice-Xcode8-iOS10
+//
+//  Created by Odin on 2016-12-12.
+//  Copyright © 2016 jonmercer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
