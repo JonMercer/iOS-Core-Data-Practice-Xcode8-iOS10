00687ccebb6e0026752bac7f5edf492113b72cc6
JonMercer <mercer.jon@gmail.com>
2016-12-22 15:58:33 -0800

    parsing JSON and putting it in Core Data

--- Home Report - With images/Home Report/AppDelegate.swift
+++ Home Report - With images/Home Report/AppDelegate.swift
@@ -42,5 +42,125 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Saves changes in the application's managed object context before the application terminates.
         coreData.saveContext()
     }
+    
+    func checkDataStore() {
+        let request: NSFetchRequest<Home> = Home.fetchRequest()
+        
+        let moc = coreData.persistentContainer.viewContext
+        
+        do {
+            let homeCount = try moc.count(for: request)
+            
+            if (homeCount == 0) {
+                uploadSampleData()
+            }
+        } catch {
+            fatalError("Error in counting home record")
+        }
+    }
+    
+    func uploadSampleData() {
+        let moc = coreData.persistentContainer.viewContext
+        
+        let url = Bundle.main.url(forResource: "homes", withExtension: "json")
+        let data = try? Data(contentsOf: url!)
+        
+        do {
+            let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+            
+            let jsonArray = jsonResult.value(forKey: "home") as! NSArray
+            
+            for json in jsonArray {
+                let homeData = json as! [String: AnyObject]
+                
+                guard let city = homeData["city"] else {
+                    return
+                }
+                
+                guard let price = homeData["price"] else {
+                    return
+                }
+                
+                guard let bed = homeData["bed"] else {
+                    return
+                }
+                
+                guard let bath = homeData["bath"] else {
+                    return
+                }
+                
+                guard let sqft = homeData["sqft"] else {
+                    return
+                }
+                
+                var image: UIImage?
+                
+                if let currentImage = homeData["image"] {
+                    let imageName = currentImage as? String
+                    image = UIImage(named: imageName!)
+                }
+                
+                guard let homeCategory = homeData["category"] else {
+                    return
+                }
+                let homeType = (homeCategory as! NSDictionary)["hometype"] as? String
+                
+                guard let homeStatus = homeData["homestatus"] else {
+                    return
+                }
+                
+                let isForsale = (homeStatus as! NSDictionary)["isForSale"] as? Bool
+                
+                
+                
+                //check type of home
+                let home = homeType?.caseInsensitiveCompare("condo") == .orderedSame ? Condo(context: moc) : SingleFamily(context: moc)
+                
+                home.price = price as! Double
+                home.bed = bed.int16Value
+                home.bath = bath.int16Value
+                home.sqft = sqft.int16Value
+                home.image = NSData.init(data: UIImageJPEGRepresentation(image!, 1.0)!)
+                home.homeType = homeType
+                home.city = city as! String
+                home.isForSale = isForsale!
+                
+                if let unitsPerBuilding = homeData["unitsPerBuilding"] {
+                    (home as! Condo).unitsPerBuilding = unitsPerBuilding.int16Value
+                }
+                
+                if let lotSize = homeData["lotSize"] {
+                    (home as! SingleFamily).lotSize = lotSize.int16Value
+                }
+                
+                if let saleHistories = homeData["saleHistory"] {
+                    let saleHistoryData = home.saleHistory?.mutableCopy() as! NSMutableSet
+                    
+                    for saleDetail in saleHistories as! NSArray {
+                        let saleData = saleDetail as! [String: AnyObject]
+                        
+                        let saleHistory = SaleHistory(context: moc)
+                        saleHistory.soldPrice = saleData["soldPrice"] as! Double
+                        
+                        let soldDateStr = saleData["soldDate"] as! String
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.dateFormat = "yyy-MM-dd"
+                        
+                        let soldDate = dateFormatter.date(from: soldDateStr)
+                        saleHistory.soldDate = soldDate
+                        
+                        saleHistoryData.add(saleHistory)
+                        
+                        home.addToSaleHistory(saleHistoryData)
+                    }
+                }
+                
+            }
+            
+            coreData.saveContext()
+        } catch {
+            fatalError("cannot upload sample data")
+        }
+    }
 }
 
